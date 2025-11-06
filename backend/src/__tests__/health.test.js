cdafb5150d1fe68d853c072162b1abc309f415d4
Zuraxy <ZuraxyOfficial@gmail.com>
2025-11-07 02:48:10 +0800

    Revert "fix: unit tests"
    This reverts commit d88690d0b3f84a66a354cba793e7d9ca503d116c.

--- backend/src/__tests__/health.test.js
+++
@@ -1,4 +0,0 @@
-import request from "node:http";
-test("dummy backend test passes", () => {
-  expect(true).toBe(true);
-});
--- frontend/src/__tests__/smoke.test.js
+++
@@ -1,6 +0,0 @@
-import { render } from "@testing-library/react";
-import Home from "../app/page.js";
-test("renders Home component", () => {
-  const { getByText } = render(<Home />);
-  expect(getByText(/Get started/i)).toBeInTheDocument();
-});
