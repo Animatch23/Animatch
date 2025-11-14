512806dbcb08d790d051993acbb2f606a9d2add7
swifted03 <franzcolobong23@gmail.com>
2025-11-14 20:49:52 +0800

    Add playwright workflow

--- frontend/playwright/tests/test_queue.py
+++ frontend/playwright/tests/test_queue.py
@@ -14,7 +14,7 @@ faker = Faker()
 
 def run():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch()
         page = browser.new_page()
         
         mock_session(page)
