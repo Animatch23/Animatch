43820fa65a2137db63003833695e41de1fcb3aa9
swifted03 <franzcolobong23@gmail.com>
2025-11-14 22:47:04 +0800

    Add exit code for playwright test

--- frontend/playwright/runner.py
+++ frontend/playwright/runner.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import sys
 
 tests_dir = os.path.join(os.path.dirname(__file__), "tests")
 
@@ -7,7 +8,12 @@ tests = [
     os.path.join(tests_dir, "test_queue.py")
 ]
 
+code = 0
+
 for test in tests:
     pytest_cmd = ["-v", test]
     exit_code = pytest.main(pytest_cmd)
-        
+    if exit_code != 0:
+        code = 1
+
+sys.exit(code)
