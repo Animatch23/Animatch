afd7631447b0316468525b57f4ee24f626efaa90
swifted03 <franzcolobong23@gmail.com>
2025-11-25 22:44:57 +0800

    Add playwright

---
+++ frontend/playwright/tests/chat.spec.js
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+import { mockSession, login } from "../helpers/login.js";
+
+test.describe("chat tests", () => {
+  test.describe.configure({
+    repeatEach: 2,
+    retries: 1,
+  });
+
+  test("chat", async ({ page }) => {
+    await mockSession(page);
+    await login(page);
+
+    await page.goto("http://localhost:3000/terms");
+    await page.getByText("Accept & Continue").click();
+
+    const username = faker.person.firstName() + Math.floor(Math.random() * 9000 + 1000);
+    await page.locator('input[placeholder="Username *"]').fill(username);
+
+    await page.getByText("Complete Setup").click();
+    await page.getByText("Start Matching").click();
+
+    // const element = await page.waitForSelector("h1:text('AniMatch Chat')", {
+    //   timeout: 10000,
+    // });
+
+    // expect(element).not.toBeNull();
+
+    await page.getByPlaceholder("Say hello...").fill("Hello");
+    const sendButton = page.getByRole("button", { name: "Send" });
+
+    await sendButton.click();
+
+    await expect(
+      page.locator("div.bg-white").filter({ hasText: "Hello" })
+    ).toBeVisible();
+  });
+});
