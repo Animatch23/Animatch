9a24970f7dfada67191f32b8ff8a0713f3037092
Mateogas <dckarlmat@gmail.com>
2025-11-27 00:11:02 +0800

    adjusted playwright test config

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./playwright/tests",
   reporter: "list",
-  repeatEach: 2,
-  workers: 2, 
+  repeatEach: 1,
+  workers: 1, 
 });
